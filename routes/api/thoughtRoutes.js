6f1e2cb52564fb3cfc6d75b219b9bcb2e04ef498
Ryan Carey <ryaanski18@yahoo.com>
2022-01-30 17:25:22 -0500

    added thought routes with future routes I may need. Created userController.js. added routes to get all users and a single user

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,15 @@
+//get all thoughts
+
+//get thoughts by user id
+
+//post to create new thought in thoughts and add to user id thoughts
+
+//put to update thought by id
+
+//delete to remove thought
+
+//thoughts/:thoughtID/reactions/
+
+//post to create a reaction that stores within a thoughts reaction array
+
+//delete to remove reaction within thoughts reaction array
