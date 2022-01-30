5a6306293f715e6fdec77e54e89914f16feb5268
Ryan Carey <ryaanski18@yahoo.com>
2022-01-29 21:57:54 -0500

    added schema for thoughts

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, Types } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
