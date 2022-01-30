89609917a60d63697115dfd575a3e6f07bb287e5
Ryan Carey <ryaanski18@yahoo.com>
2022-01-30 17:23:51 -0500

    added a userRoutes.js file with all routes I will need

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  updateFriend,
+  deleteFriend,
+} = require("../../controllers/userController.js");
+
+//api/users
+router.route("/").get(getUsers).post(createUser);
+
+//api/users/:userID
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+//api/users/:userId/friend/:freindId
+router
+  .route("/:userId/friend/:friendId")
+  .put(updateFriend)
+  .delete(deleteFriend);
+//post user to user friend list, delete user from user friend list
