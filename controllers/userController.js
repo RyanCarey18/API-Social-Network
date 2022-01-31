2371717963dc11d7db056b9f929b02140588d492
Ryan Carey <ryaanski18@yahoo.com>
2022-01-30 19:35:44 -0500

    added updatefriend route which lets you add friends to your friend list

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,4 +52,18 @@ module.exports = {
       .then(() => res.json({ message: "User and thoughts deleted!" }))
       .catch((err) => res.status(500).json(err));
   },
+  //Update Friends List
+  updateFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that Id." })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
